433f6db9a68b3d0c96347e9574647d74ec74a5f2
kurotokiya <kurotokiya@gmail.com>
2016-02-04 20:56:43 +0800

    fix missing rewrite.conf

--- php-legacy/run.sh
+++ php-legacy/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [[ ! -e /etc/nginx/rewrite.conf ]]; then
+  touch /etc/nginx/rewrite.conf
+fi
+
 nginx
 /usr/local/php/sbin/php-fpm -F
--- php/run.sh
+++ php/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [[ ! -e /etc/nginx/rewrite.conf ]]; then
+  touch /etc/nginx/rewrite.conf
+fi
+
 nginx
 php-fpm -F
